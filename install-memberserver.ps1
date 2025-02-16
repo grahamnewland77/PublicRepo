81c9830e76a1b8dab909c478a84962215042c321
grahamnewland77 <122306114+grahamnewland77@users.noreply.github.com>
2025-02-16 18:57:20 +0000

    Create install-memberserver.ps1

---
+++ install-memberserver.ps1
@@ -0,0 +1,29 @@
+# Install-Application.ps1
+#
+# -- Graham Newland <graham@grahamnewland.co.uk>
+#
+# This script needs to be hosted publicly (or in an Azure Storage account), and
+# the path and name of the script need be given in DeployCustomer.ps1, eg:
+#
+# $scriptUri = "https://raw.githubusercontent.com/grahamnewland77/PublicRepo/refs/heads/main/install-notepadplusplus.ps1"
+# $commandToExecute = "powershell -ExecutionPolicy Unrestricted -File install-notepadplusplus.ps1"
+# 
+# The script will then be executed once the VM has been deployed.
+#
+
+param (
+    [string]$gateway,
+    [string]$privateIP,
+    [string]$dnsprimary,
+    [string]$dnssecondary
+)
+start-transcript -path c:\customscript.txt
+#$plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+$interface = get-netadapter | where-object { $_.Status -eq "Up" }
+New-NetIPAddress -interfaceIndex $interface.ifindex -IPAddress $privateIP -PrefixLength 24 -DefaultGateway $gateway
+#new-route -destinationprefix "0.0.0.0/0" -interfaceindex (get-netadapter).ifindex -NextHop $gateway
+Set-DnsClientServerAddress -InterfaceIndex $interface.ifIndex -ServerAddresses ($dnsPrimary, $dnsSecondary)
+
+stop-transcript
+#Restart-Computer -force
